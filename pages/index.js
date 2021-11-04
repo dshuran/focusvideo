59daa46e8d6f88803dac5ed115e51334c0fa36bd
Dmitry Shuran <dmitry.shuran@gmail.com>
2021-11-04 18:23:55 +0300

    styling site with tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 
 export default function Home() {
   return (
-      <div className='mx-auto'>
-          <div className='mx-auto bg-gray-500'>
-              Hello
+      <div className='bg-yellow-300 flex justify-center'>
+          <div className='max-w-2xl flex-grow h-60 mt-10 flex flex-col'>
+              <input
+                  type='text'
+                  placeholder='Paste video link there'
+                  className='mb-16 rounded-2xl p-2 pl-4 border focus:border-blue-600 text-gray-700 shadow text-lg appearance-none font-semibold focus:outline-none focus:shadow-outline'
+              />
+              <div className='bg-gray-500'>
+                  Block for video
+              </div>
           </div>
       </div>
   )
