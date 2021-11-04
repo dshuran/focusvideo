5f7c1dbf6476737af17fa53a274d0c2502ca413d
Dmitry Shuran <dmitry.shuran@gmail.com>
2021-11-04 18:45:03 +0300

    added youtube video

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
+import YouTube from "react-youtube";
 
 export default function Home() {
-  return (
+    const opts = {
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    };
+
+    return (
       <div className='bg-yellow-300 flex justify-center'>
-          <div className='max-w-2xl flex-grow h-60 mt-10 flex flex-col'>
+          <div className='max-w-2xl flex-grow mt-10 flex flex-col'>
               <input
                   type='text'
                   placeholder='Paste video link there'
                   className='mb-16 rounded-2xl p-2 pl-4 border focus:border-blue-600 text-gray-700 shadow text-lg appearance-none font-semibold focus:outline-none focus:shadow-outline'
               />
-              <div className='bg-gray-500'>
-                  Block for video
+              <div className='flex justify-center'>
+                  <YouTube videoId="1PetRVcM2sk" opts={opts} className='flex-grow'/>
               </div>
           </div>
       </div>
-  )
+    )
 }
